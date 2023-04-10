ea9f1f93a104c422877ce95d8634794c2378d757
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-04-10 22:30:30 +0200

    Add test for FileStream

--- src/lexer/tests/test_streams.py
+++ src/lexer/tests/test_streams.py
@@ -1,8 +1,10 @@
-from lexer.streams import TextStream
+import pytest
+
+from lexer.streams import TextStream, FileStream
 
 
-def test_text_stream():
-    text = (
+TEST_SAMPLES = (
+    (
         "€var = 15;!\n",
         "match (input1, input2) {~\n",
         ">   (Str as name, Num as age): {\n",
@@ -15,15 +17,37 @@ def test_text_stream():
         "   }\n",
         '   (_, _): print("Invalid input");\n',
         "}",
-    )
-    stream = TextStream("".join(text))
+    ),
+)
+
+
+def _test_stream(stream, text):
     assert stream.current_char == ''
     assert stream.position.line == 1
     assert stream.position.column == 0
+    assert stream.position.offset == 0
 
+    offset = 0
     for line_n, line in enumerate(text):
         for col_n, char in enumerate(line):
             stream.advance()
             assert stream.current_char == char
             assert stream.position.line == line_n + 1
             assert stream.position.column == col_n + 1
+            assert stream.position.offset == offset
+            offset += len(stream.current_char.encode('utf-8'))
+
+
+@pytest.mark.parametrize('text', TEST_SAMPLES)
+def test_text_stream(text):
+    stream = TextStream("".join(text))
+    _test_stream(stream, text)
+
+
+@pytest.mark.parametrize('text', TEST_SAMPLES)
+def test_file_stream(text, tmpdir):
+    file = tmpdir.join("test.txt")
+    file.write("".join(text))
+    with open(file, "r") as f:
+        stream = FileStream(f)
+        _test_stream(stream, text)
