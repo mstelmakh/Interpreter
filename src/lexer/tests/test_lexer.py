992cce7106c9f1d32fa79eb77753ee810740185b
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-04-11 20:57:14 +0200

    Parametrize some tests

--- src/lexer/tests/test_lexer.py
+++ src/lexer/tests/test_lexer.py
@@ -2,7 +2,7 @@ import pytest
 
 from lexer.lexers import Lexer
 from lexer.streams import TextStream, Position
-from lexer.tokens import Token, TokenType
+from lexer.tokens import Token, TokenType, COMPOSITE_CHAR_MAP, SINGLE_CHAR_MAP
 from lexer.exceptions import LexerError
 from lexer.tests.utils import get_all_tokens, DebugErrorHandler, create_lexer
 
@@ -44,38 +44,54 @@ def test_lexer(text: str, tokens: list[Token], errors: list[LexerError]):
     assert error_handler.errors == errors
 
 
-def test_skip_whitespaces():
-    text = "\t     \t\n  \r  abc"
-    lexer = create_lexer(text)
-
-    assert lexer.stream.current_char == "\t"
-    lexer.skip_whitespaces()
-    assert lexer.stream.current_char == "a"
-    lexer.skip_whitespaces()
-    assert lexer.stream.current_char == "a"
-
-
-def test_try_build_operator_or_comment_fail():
-    text = "hello"
-    lexer = create_lexer(text)
-
-    assert lexer.stream.current_char == "h"
+def test_init_lexer():
+    text = "var a = 5;"
+    stream = TextStream(text)
+    error_handler = DebugErrorHandler()
+    lexer = Lexer(stream, error_handler)
+    assert lexer.stream.current_char == "v"
     assert lexer.token is None
+
+
+@pytest.mark.parametrize('ident', (
+        '\t     \t\n  \r ', '\r \t \n    ',
+        '\r\r\r', '   \n\r\t', ''
+))
+def test_skip_whitespaces(ident):
+    text = ident + "abc"
+    lexer = create_lexer(text)
+
+    lexer.skip_whitespaces()
+    assert lexer.stream.current_char == "a"
+    lexer.skip_whitespaces()
+    assert lexer.stream.current_char == "a"
+
+
+@pytest.mark.parametrize('text', (
+        'a+-=*', '"string"+-=*', '5+-=*',
+        ' +-=*', 'fn+-=*', '\n+-=*'
+        )
+)
+def test_try_build_operator_or_comment_fail(text: str):
+    lexer = create_lexer(text)
+
     assert lexer.try_build_operator_or_comment() is False
-    assert lexer.stream.current_char == "h"
+    assert lexer.stream.current_char == text[0]
     assert lexer.token is None
 
 
-def test_try_build_operator_or_comment_plus():
-    text = "+abcdef"
+@pytest.mark.parametrize('operator, token_type', SINGLE_CHAR_MAP.items())
+def test_try_build_operator_or_comment_plus(
+    operator: str,
+    token_type: TokenType
+):
+    text = operator + "abcdef"
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "+"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "a"
     assert lexer.token is not None
-    assert lexer.token.type == TokenType.PLUS
+    assert lexer.token.type == token_type
     assert lexer.token.value is None
 
 
@@ -83,8 +99,6 @@ def test_try_build_operator_or_comment_slash():
     text = "/abcdef"
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "/"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "a"
     assert lexer.token is not None
@@ -96,8 +110,6 @@ def test_try_build_operator_or_comment_part_of_composite():
     text = "=abcdef"
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "="
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "a"
     assert lexer.token is not None
@@ -109,8 +121,6 @@ def test_try_build_operator_or_comment_part_of_composite_error():
     text = "!abcdef"
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "!"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "a"
     assert lexer.token is None
@@ -120,16 +130,18 @@ def test_try_build_operator_or_comment_part_of_composite_error():
     ]
 
 
-def test_try_build_operator_or_comment_composite():
-    text = "<=abcdef"
+@pytest.mark.parametrize('operator, token_type', COMPOSITE_CHAR_MAP.items())
+def test_try_build_operator_or_comment_composite(
+    operator: str,
+    token_type: TokenType
+):
+    text = operator + "a-c$e\n"
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "<"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "a"
     assert lexer.token is not None
-    assert lexer.token.type == TokenType.LESS_EQUAL
+    assert lexer.token.type == token_type
     assert lexer.token.value is None
 
 
@@ -137,8 +149,6 @@ def test_try_build_operator_or_comment_comment():
     text = '//ab"e\tf'
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "/"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == ""
     assert lexer.token is not None
@@ -150,10 +160,67 @@ def test_try_build_operator_or_comment_comment_with_newline():
     text = '//ab"e\tf\nafter'
     lexer = create_lexer(text)
 
-    assert lexer.stream.current_char == "/"
-    assert lexer.token is None
     assert lexer.try_build_operator_or_comment() is True
     assert lexer.stream.current_char == "\n"
     assert lexer.token is not None
     assert lexer.token.type == TokenType.COMMENT
     assert lexer.token.value == 'ab"e\tf'
+
+
+@pytest.mark.parametrize('text', (
+        'a"string"', '="string"', '//"string"', '5"string"',
+        ' "string"', 'fn"string"', '\n"string"'
+        )
+)
+def test_try_build_string_fail(text):
+    lexer = create_lexer(text)
+
+    assert lexer.try_build_string() is False
+    assert lexer.stream.current_char == text[0]
+    assert lexer.token is None
+
+
+@pytest.mark.parametrize('string_value', (
+        "text", "53+52", "'text'", "%^&!2", "\nabc\t\n"
+))
+def test_try_build_string_normal(string_value: str):
+    text = '"' + string_value + '"' + "after"
+    lexer = create_lexer(text)
+
+    assert lexer.try_build_string() is True
+    assert lexer.stream.current_char == "a"
+    assert lexer.token is not None
+    assert lexer.token.type == TokenType.STRING
+    assert lexer.token.value == string_value
+
+
+def test_try_build_string_with_escape_character():
+    pass
+
+
+def test_try_build_string_with_invalid_escape_character():
+    pass
+
+
+def test_try_build_string_unterminated():
+    pass
+
+
+def test_try_build_number_fail():
+    pass
+
+
+def test_try_build_number_integer():
+    pass
+
+
+def test_try_build_number_float():
+    pass
+
+
+def test_try_build_number_with_leading_zeros():
+    pass
+
+
+def test_try_build_number_without_float_part():
+    pass
