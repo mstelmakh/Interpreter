a64b26800ac8df2dc4e9dbe57021bdc6bb6c5e32
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-04-12 13:11:28 +0200

    Fix util functions

--- src/lexer/utils.py
+++ src/lexer/utils.py
@@ -1,8 +1,12 @@
 def is_digit(c: str) -> bool:
+    if not len(c) == 1:
+        return False
     return c >= '0' and c <= '9'
 
 
 def is_alpha(c: str) -> bool:
+    if not len(c) == 1:
+        return False
     return (
         (c >= 'a' and c <= 'z') or
         (c >= 'A' and c <= 'Z') or
@@ -11,4 +15,6 @@ def is_alpha(c: str) -> bool:
 
 
 def is_alphanumeric(c: str) -> bool:
+    if not len(c) == 1:
+        return False
     return is_alpha(c) or is_digit(c)
