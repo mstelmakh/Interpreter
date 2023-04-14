2f67b55f040c0a7d48b6a4cd6add786628975237
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-04-14 16:48:50 +0200

    Update next_token() to return token and remove error handler

--- src/lexer/lexers.py
+++ src/lexer/lexers.py
@@ -10,9 +10,12 @@ from lexer.tokens import (
     COMMENT_CHAR
 )
 from lexer.utils import is_digit, is_alpha, is_alphanumeric
-from lexer.streams import Stream, Position
-from error_handlers import BaseErrorHandler
-from lexer.exceptions import LexerError
+from lexer.streams import Stream
+from lexer.exceptions import (
+    UnexpecterCharacterError,
+    UnterminatedStringError,
+    InvalidEscapeSequenceError
+)
 
 from abc import ABC, abstractmethod
 
@@ -25,25 +28,15 @@ class BaseLexer(ABC):
 
 
 class Lexer(BaseLexer):
-    def __init__(self, stream: Stream, error_handler: BaseErrorHandler):
+    def __init__(self, stream: Stream):
         self.stream = stream
-        self.error_handler = error_handler
-        self.token: Token | None = None
 
         self.stream.advance()
-        self.start_position = copy(self.stream.position)
-
-    def error(self, message: str, position: Position | None = None) -> None:
-        self.token = None
-        error = LexerError(
-            message,
-            self.start_position if not position else position
-        )
-        self.error_handler.handle_lexer_error(error)
+        self.lexeme_start_pos = copy(self.stream.position)
 
     def try_build_string(self) -> bool:
         if not self.stream.current_char == '"':
-            return False
+            return None
         self.stream.advance()
         value = []
         while self.stream.current_char and not self.stream.current_char == '"':
@@ -51,8 +44,7 @@ class Lexer(BaseLexer):
                 position = copy(self.stream.position)
                 self.stream.advance()
                 if self.stream.current_char == "":
-                    self.error("Unterminated string.")
-                    return True
+                    raise UnterminatedStringError(self.lexeme_start_pos)
                 if self.stream.current_char == "n":
                     value.append("\n")
                 elif self.stream.current_char == "b":
@@ -66,27 +58,24 @@ class Lexer(BaseLexer):
                 elif self.stream.current_char == "\"":
                     value.append("\"")
                 else:
-                    self.error(
-                        "Invalid escape character: " +
-                        f"'\\{self.stream.current_char}'",
-                        position
+                    raise InvalidEscapeSequenceError(
+                        sequence=self.stream.current_char,
+                        position=position
                     )
             else:
                 value.append(self.stream.current_char)
             self.stream.advance()
         if not self.stream.current_char:
-            self.error("Unterminated string.")
-            return True
+            raise UnterminatedStringError(self.lexeme_start_pos)
         if self.stream.current_char == '"':
             self.stream.advance()
-        self.token = self.create_token(TokenType.STRING, "".join(value))
-        return True
+        return self.create_token(TokenType.STRING, "".join(value))
 
     def try_build_number(self) -> bool:
         # Allows leading zeros and float number without float part
         # For example: 000015.5 = 15.5, 15. = 15.0
         if not is_digit(self.stream.current_char):
-            return False
+            return None
         integer = 0
         fraction = 0.0
         fraction_length = 0
@@ -103,28 +92,25 @@ class Lexer(BaseLexer):
                 self.stream.advance()
         if fraction_length > 0:
             fraction = fraction / (10 ** fraction_length)
-        self.token = self.create_token(
+        return self.create_token(
             TokenType.NUMBER,
             integer + fraction if is_float else integer
         )
-        return True
 
     def try_build_ident_or_keyword(self) -> bool:
         if not is_alpha(self.stream.current_char):
-            return False
+            return None
         value = []
         while is_alphanumeric(self.stream.current_char):
             value.append(self.stream.current_char)
             self.stream.advance()
         value_str = "".join(value)
         if value_str in KEYWORDS_MAP:
-            self.token = self.create_token(KEYWORDS_MAP[value_str])
-        else:
-            self.token = self.create_token(
-                TokenType.IDENTIFIER,
-                value_str
-            )
-        return True
+            return self.create_token(KEYWORDS_MAP[value_str])
+        return self.create_token(
+            TokenType.IDENTIFIER,
+            value_str
+        )
 
     def build_comment(self) -> Token:
         value = []
@@ -133,48 +119,54 @@ class Lexer(BaseLexer):
             self.stream.advance()
         return self.create_token(TokenType.COMMENT, "".join(value))
 
-    def try_build_operator_or_comment(self) -> bool:
-        # TODO
-        is_beginning_of_composite = (
-            self.stream.current_char
-            in [string[0] for string in COMPOSITE_CHAR_MAP.keys()]
+    def _is_beginning_of_composite_operator_or_comment(self) -> bool:
+        char = self.stream.current_char
+        return (
+            char in [string[0] for string in COMPOSITE_CHAR_MAP.keys()]
+            or char == COMMENT_CHAR[0]
         )
-        is_beginning_of_comment = self.stream.current_char == COMMENT_CHAR[0]
-        if not (self.stream.current_char in SINGLE_CHAR_MAP
-                or is_beginning_of_composite):
-            return False
-        if not (is_beginning_of_composite or is_beginning_of_comment):
-            self.token = self.create_token(
+
+    def try_build_operator_or_comment(self) -> bool:
+        is_single_char_operator = self.stream.current_char in SINGLE_CHAR_MAP
+        is_beginning_of_composite_or_comment = \
+            self._is_beginning_of_composite_operator_or_comment()
+        if not (is_single_char_operator or
+                is_beginning_of_composite_or_comment
+                ):
+            return None
+        if not is_beginning_of_composite_or_comment:
+            token = self.create_token(
                 SINGLE_CHAR_MAP[self.stream.current_char]
             )
             self.stream.advance()
-            return True
+            return token
         previous = self.stream.current_char
         self.stream.advance()
         lexeme = previous + self.stream.current_char
         if lexeme == COMMENT_CHAR:
             self.stream.advance()
-            self.token = self.build_comment()
+            token = self.build_comment()
         elif lexeme in COMPOSITE_CHAR_MAP:
-            self.token = self.create_token(COMPOSITE_CHAR_MAP[lexeme])
+            token = self.create_token(COMPOSITE_CHAR_MAP[lexeme])
             self.stream.advance()
         elif previous in SINGLE_CHAR_MAP:
             # If the first character was the beginning of the composite
             # operator, but the second character didn't match.
             # For example if '=' and '==' are operators, but we have '=-'
-            self.token = self.create_token(SINGLE_CHAR_MAP[previous])
+            token = self.create_token(SINGLE_CHAR_MAP[previous])
         else:
             # If started as a composite operator, but didn't match any
             # and is not a single char operator
-            self.error(f"Unexpected character: {previous}")
-            return True
-        return True
+            raise UnexpecterCharacterError(
+                character=previous,
+                position=self.lexeme_start_pos
+            )
+        return token
 
     def try_build_eof(self) -> bool:
         if not self.stream.current_char:
-            self.token = self.create_token(TokenType.EOF)
-            return True
-        return False
+            return self.create_token(TokenType.EOF)
+        return None
 
     def skip_whitespaces(self) -> None:
         while self.stream.current_char in INDENTATION_CHARS:
@@ -189,22 +181,23 @@ class Lexer(BaseLexer):
             self.try_build_eof
         )
         self.skip_whitespaces()
-        self.start_position = copy(self.stream.position)
+        self.lexeme_start_pos = copy(self.stream.position)
         for func in funcs:
-            # token = func()
-            if func():
-                return self.token
+            token = func()
+            if token:
+                return token
 
-        self.error(f"Unexpected character: {self.stream.current_char}")
-        self.stream.advance()
-        return None
+        raise UnexpecterCharacterError(
+            character=self.stream.current_char,
+            position=self.lexeme_start_pos
+        )
 
     def create_token(
         self,
         token_type: TokenType,
         value: int | float | str | None = None
     ) -> None:
-        return Token(token_type, value, self.start_position)
+        return Token(token_type, value, self.lexeme_start_pos)
 
 
 class LexerWithoutComments(BaseLexer):
@@ -215,5 +208,4 @@ class LexerWithoutComments(BaseLexer):
         token = self.lexer.next_token()
         while token and token.type == TokenType.COMMENT:
             token = self.lexer.next_token()
-        self.token = self.lexer.token
-        return self.token
+        return token
