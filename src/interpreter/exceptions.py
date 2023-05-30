2ebba3da67cbcf2a5613db4ce602b47c8ebebadf
Mikalai Stelmakh <mikalai.stelmakh@gmail.com>
2023-05-30 21:55:28 +0200

    Add interpreter exceptions

--- src/interpreter/exceptions.py
+++ src/interpreter/exceptions.py
@@ -1,3 +1,69 @@
-class Return(RuntimeError):
+from lexer.streams import Position
+
+
+class Return(Exception):
     def __init__(self, value: str | int | float | bool | None):
         self.value = value
+
+
+class RuntimeError(Exception):
+    def __init__(self, message: str, position: Position):
+        super().__init__(message)
+        self.position = position
+
+
+class UndefinedVariableError(RuntimeError):
+    def __init__(self, name: str, position: Position):
+        super().__init__(
+            f"Undefined variable '{name}'",
+            position
+        )
+
+
+class UndefinedFunctionError(RuntimeError):
+    def __init__(self, name: str, position: Position):
+        super().__init__(
+            f"Undefined function '{name}'",
+            position
+        )
+
+
+class RedefinitionError(RuntimeError):
+    def __init__(self, name: str, position: Position):
+        super().__init__(
+            f"'{name}' is already defined",
+            position
+        )
+
+
+class ConstantRedefinitionError(RuntimeError):
+    def __init__(self, name: str, position: Position):
+        super().__init__(
+            f"Cannot redefine constant '{name}'",
+            position
+        )
+
+
+class NumberConversionError(RuntimeError):
+    def __init__(self, value: str, position: Position):
+        super().__init__(
+            f"Cannot convert '{value}' to a number",
+            position
+        )
+
+
+class DivisionByZeroError(RuntimeError):
+    def __init__(self, position: Position):
+        super().__init__(
+            "Division by zero",
+            position
+        )
+
+
+class InvalidArgumentNumberError(RuntimeError):
+    def __init__(self, name: str, expected: int, got: int, position: Position):
+        super().__init__(
+            f"Invalid number of arguments for '{name}': "
+            f"expected {expected}, got {got}",
+            position
+        )
